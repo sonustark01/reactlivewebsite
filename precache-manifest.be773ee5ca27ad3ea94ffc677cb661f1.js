a79ea901c6f2824b55bef81656f7942861d4cfd5
sonustark01 <collegegameer@gmail.com>
2023-03-12 17:01:43 +0530

    Updates

---
+++ precache-manifest.be773ee5ca27ad3ea94ffc677cb661f1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a6e3ba05cf7b0799297",
+    "url": "/textutilityapp/static/js/main.3a6e3ba0.chunk.js"
+  },
+  {
+    "revision": "ffc0cc4d93b1c17992c5",
+    "url": "/textutilityapp/static/js/1.ffc0cc4d.chunk.js"
+  },
+  {
+    "revision": "4d67c2be37f1e1cb3cbf",
+    "url": "/textutilityapp/static/js/runtime~main.4d67c2be.js"
+  },
+  {
+    "revision": "2686404f1c0289b40d9b23dd7e2b4d20",
+    "url": "/textutilityapp/index.html"
+  }
+];
